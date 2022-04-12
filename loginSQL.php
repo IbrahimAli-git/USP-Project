e9e6a94c27158b73d1ad32c7a3d9e41a9b482424
JacobSmith333 <101271718+JacobSmith333@users.noreply.github.com>
2022-04-12 15:29:47 +0100

    Add files via upload

---
+++ loginSQL.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+$uname = $_POST["Username1"];
+$pword = $_POST["Password1"];
+
+$db = new SQLite3('C:\xampp\Data\USPdata.db');
+$sql = "SELECT COUNT(*) as count FROM Users WHERE username = :user AND password = :pass";
+$stmt = $db->prepare($sql);
+$stmt->bindParam(':user', $uname, SQLITE3_TEXT);
+$stmt->bindParam(':pass', $pword, SQLITE3_TEXT);
+$result = $stmt->execute();
+$row = $result->fetchArray();
+$count=$row['count'];
+if($count > 0){
+    $db = new SQLite3('C:\xampp\Data\APEDatabase.db');
+    $sql = "SELECT username, userrole, userID FROM users WHERE username = :user AND pword = :pass";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':user', $uname, SQLITE3_TEXT);
+    $stmt->bindParam(':pass', $pword, SQLITE3_TEXT);
+    $result = $stmt->execute();
+    $row = $result->fetchArray();
+
+    $_SESSION["id"] = $row[1];
+
+    header('location: HomePage.html');
+}
+
+else{
+     header('location: login.html');
+}
+    
